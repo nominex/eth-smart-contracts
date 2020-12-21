dac6a3c0b924c036f4038ba4f51d045d1b40f5ba
Andrey Vinichenko <4vinichenko@gmail.com>
2020-12-21 20:35:43 +0400

    Add comments.

--- contracts/V02_Lib.sol
+++ contracts/V02_Lib.sol
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.12 <0.9.0;
 
+/**
+ * @dev kinds of possible pools
+ *
+ * DEFAULT_VALUE - dummy type for null value
+ * PRIMARY - blockchain based staking. All rules are declared in this smart contract
+ * BONUS - Nominex platform based staking. All rules are declared and implemented in the Nominex business logic
+ * TEAM - Nominex partner structure platform based staking. All rules are declared and implemented in the Nominex business logic
+ * NOMINEX - tokens for Nominex company
+ */
 enum MintPool {DEFAULT_VALUE, PRIMARY, BONUS, TEAM, NOMINEX}
--- contracts/V02_StakingService.sol
+++ contracts/V02_StakingService.sol
@@ -8,24 +8,61 @@ import "abdk-libraries-solidity/ABDKMath64x64.sol";
 
 contract StakingService is PausableByOwner {
     using ABDKMath64x64 for int128;
+
     struct State {
+        /**
+         * @dev how many NMX rewards for one NMXLP
+         */
         int128 historicalRewardRate;
+        /**
+         * @dev how many NMXLP are staked in total
+         */
         int128 totalStaked;
     }
 
     struct Staker {
+        /**
+         * @dev how much NMXLP staked
+         */
         int128 amount;
+        /**
+         * @dev rice at which the reward was last paid
+         */
         int128 initialRewardRate;
     }
 
+    /**
+     * @dev Nmx contract
+     */
     address nmx;
+    /**
+     * @dev ERC20 TODO
+     */
     address stakingToken;
+    /**
+     * @dev to got minted NMX
+     */
     address nmxSupplier;
+    /**
+     * @dev internal service state
+     */
     State state;
+    /**
+     * @dev mapping a staker's address to his state
+     */
     mapping(address => Staker) stakers;
 
+    /**
+     * @dev event when someone is staked NMXLP
+     */
     event Staked(address indexed owner, uint256 amount);
+    /**
+     * @dev event when someone unstaked NMXLP
+     */
     event Unstaked(address indexed owner, uint256 amount);
+    /**
+     * @dev event when someone is awarded NMX
+     */
     event Rewarded(address indexed owner, uint256 amount);
 
     constructor(
@@ -39,6 +76,12 @@ contract StakingService is PausableByOwner {
         state.totalStaked = 1; // to avoid division by zero on the first stake
     }
 
+    /**
+     * @dev accepts NMXLP staked by the user, also rewards for the previously staked amount at the current rate
+     * emit Staked and Rewarded events
+     *
+     * amount - new part of staked NMXLP
+     */
     function stake(uint256 amount) external whenNotPaused {
         bool transferred =
             IERC20(stakingToken).transferFrom(
@@ -57,6 +100,12 @@ contract StakingService is PausableByOwner {
         staker.amount += amount;
     }
 
+    /**
+     * @dev accepts NMXLP unstaked by the user, also rewards for the previously staked amount at the current rate
+     * emit Unstaked and Rewarded events
+     *
+     * amount - new part of staked NMXLP
+     */
     function unstaked(uint256 amount) external {
         Staker storage staker = stakers[msg.sender];
         require(staker.amount >= amount, "NMXSTKSRV: NOT_ENOUGH_STAKED");
@@ -71,11 +120,20 @@ contract StakingService is PausableByOwner {
         staker.amount += amount;
     }
 
+    /**
+     * @dev rewards for the previously staked amount at the current rate
+     * emit Rewarded event
+     *
+     * amount - new part of staked NMXLP
+     */
     function reward(address owner) external {
         updateHistoricalRewardRate();
         _reward(owner, stakers[owner]);
     }
 
+    /**
+     * @dev TODO
+     */
     function _reward(address owner, Staker storage staker) private {
         int128 unrewarded = state.historicalRewardRate.sub(staker.initialRewardRate).staker.amount;
         emit Rewarded(owner, unrewarded);
@@ -84,6 +142,9 @@ contract StakingService is PausableByOwner {
         staker.initialRewardRate = state.historicalRewardRate;
     }
 
+    /**
+     * @dev update how many NMX rewards for one NMXLP are currently
+     */
     function updateHistoricalRewardRate() public {
         uint256 currentNmxSupply = NmxSupplier(nmxSupplier).supplyNmx();
         if (currentNmxSupply == 0) return;
