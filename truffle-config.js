4701bc6a12411fbd692e441fa2bf83a7d7fb4546
Yuriy Plotnikov <plyuriy@gmail.com>
2021-01-19 17:00:37 +0400

    "runs" solc optimization parameter was changed to 999999

--- truffle-config.js
+++ truffle-config.js
@@ -104,7 +104,7 @@ module.exports = {
             settings: {
                 optimizer: {
                     enabled: true,
-                    runs: 10000
+                    runs: 999999
                 }
             }
         }
