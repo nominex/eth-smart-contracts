c1e8b2379b5db3ddcad51352c2f90b9ce2b05fa8
Andrey Vinichenko <4vinichenko@gmail.com>
2021-01-26 14:20:58 +0400

    Added tests for StakingService.setReferralMultiplier.

---
+++ test/stakingService/setReferralMultiplier.js
@@ -0,0 +1,61 @@
+const MockedStakingToken = artifacts.require("MockedStakingToken");
+const MockedNmxToken = artifacts.require("MockedNmxToken");
+const StakingService = artifacts.require("StakingService");
+const {rpcCommand} = require("../utils.js");
+
+contract('StakingService#setReferralMultiplier', (accounts) => {
+
+    let stakingService;
+    let snapshotId;
+
+    before(async () => {
+        let nmx = await MockedNmxToken.new();
+        let stakingToken = await MockedStakingToken.new();
+        stakingService = await StakingService.new(nmx.address, stakingToken.address, nmx.address);
+    });
+
+    beforeEach(async () => {
+        // snaphot must be taken before each test because of the issue in ganache
+        // evm_revert also deletes the saved snapshot
+        // https://github.com/trufflesuite/ganache-cli/issues/138
+        snapshotId = await rpcCommand("evm_snapshot");
+    });
+
+    afterEach(async () => {
+        await rpcCommand("evm_revert", [snapshotId]);
+    });
+
+    it('values stored', async () => {
+        await setReferralMultiplierAndVerify(5);
+    });
+
+    it('cleared values', async () => {
+        await stakingService.setReferralMultiplier(6);
+        await setReferralMultiplierAndVerify(0);
+    });
+
+    it('available when StakingService paused', async () => {
+        await stakingService.pause();
+        await stakingService.setReferralMultiplier(7);
+    });
+
+    it('available for owner', async () => {
+        await stakingService.setReferralMultiplier(0, {from: accounts[0]});
+    });
+
+    it('not available for not owner', async () => {
+        try {
+            await stakingService.setReferralMultiplier(0, {from: accounts[1]});
+            assert.fail("Error not occurred");
+        } catch (e) {
+            assert(e.message.includes("Ownable: caller is not the owner"), `Unexpected error message: ${e.message}`);
+        }
+    });
+
+    async function setReferralMultiplierAndVerify(newMultiplier) {
+        await stakingService.setReferralMultiplier(newMultiplier);
+        let actual = await stakingService.referralMultiplier();
+        assert.equal(actual.toString(), newMultiplier.toString(), "multiplier");
+    }
+
+});
