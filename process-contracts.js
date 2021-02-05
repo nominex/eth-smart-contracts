531ed53b1e20487fb0af70c2d31f1f9cad5620cc
Alexey Prokofiev <mozg1024@gmail.com>
2021-02-05 10:08:20 +0400

    add extra-files to dist folder

--- process-contracts.js
+++ process-contracts.js
@@ -20,49 +20,56 @@ const makeDirIfDoesntExist = (dirName) => {
     }
 };
 
-const buildFolder = './build/contracts';
 const abiFolder = './dist/abi';
 const contractFolder = './dist/contracts';
+
+const processFile = (sourceFolder, fileName) => {
+    if (!fileName.includes('.json')) {
+        return;
+    }
+
+    const contract = require(`${sourceFolder}/${fileName}`);
+
+    if (contract && contract.abi) {
+        console.log(`Processing ${fileName}...`);
+
+        const abiJson = JSON.stringify(contract.abi, null, 2);
+        const abiFileName = `${abiFolder}/${fileName}`;
+
+        const contractJson = JSON.stringify(contract, null, 2);
+        const contractFileName = `${contractFolder}/${fileName}`;
+
+        fs.writeFile(abiFileName, abiJson, (err) => {
+            if (err) {
+                return console.log(err);
+            }
+        });
+
+        fs.writeFile(contractFileName, contractJson, (err) => {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    }
+}
+
+const buildFolder = './build/contracts';
+const extraFiles = [{
+    sourceFolder: './node_modules/@uniswap/v2-periphery/build',
+    fileName: 'IUniswapV2Router02.json'
+}];
+
+makeDirIfDoesntExist(abiFolder);
+makeDirIfDoesntExist(contractFolder);
 
 fs.readdir(buildFolder, (err, files) => {
     if (!files || !files.length) {
         console.log('Compile contracts first!');
         return;
     }
-
-    makeDirIfDoesntExist(abiFolder);
-    makeDirIfDoesntExist(contractFolder);
-
-    files.forEach(fileName => {
-        if (!fileName.includes('.json')) {
-            return;
-        }
-
-        const contract = require(`${buildFolder}/${fileName}`);
-
-        if (contract && contract.abi) {
-            console.log(`Processing ${fileName}...`);
-
-            const abiJson = JSON.stringify(contract.abi, null, 2);
-            const abiFileName = `${abiFolder}/${fileName}`;
-
-            const contractJson = JSON.stringify(contract, null, 2);
-            const contractFileName = `${contractFolder}/${fileName}`;
-
-            fs.writeFile(abiFileName, abiJson, (err) => {
-                if (err) {
-                    return console.log(err);
-                }
-            });
-
-            fs.writeFile(contractFileName, contractJson, (err) => {
-                if (err) {
-                    return console.log(err);
-                }
-            });
-        }
-    });
-
-    console.log('Completed.');
+    files.forEach((fileName) => processFile(buildFolder, fileName));
 });
 
+extraFiles.forEach(({ sourceFolder, fileName }) => processFile(sourceFolder, fileName));
+
+console.log('Completed.');
