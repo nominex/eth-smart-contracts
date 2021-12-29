c40df3fceee274c5f625833a8103cb2f51994c97
Yuriy Plotnikov <y.plotnikov@nominex.io>
2021-12-29 23:49:28 +0400

    DoubleSupplyStakingRouter should deploy FixedRateNmxSupplier in constructor to have immutable field and avoid circular dependency

--- contracts/DoubleSupplyStakingRouter.sol
+++ contracts/DoubleSupplyStakingRouter.sol
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.8.0;
 
+import "./FixedRateNmxSupplier.sol";
 import "./StakingRouter.sol";
 
 contract DoubleSupplyStakingRouter is StakingRouter {
     address immutable public additionalSupplier;
 
-    constructor(address _nmx, address _additionalSupplier) StakingRouter(_nmx) {
-        additionalSupplier = _additionalSupplier;
+    constructor(address _nmx) StakingRouter(_nmx) {
+        FixedRateNmxSupplier fixedRateNmxSupplier = new FixedRateNmxSupplier(_nmx, address(this));
+        fixedRateNmxSupplier.transferOwnership(msg.sender);
+        additionalSupplier = address(fixedRateNmxSupplier);
     }
 
     function receiveSupply(uint40 maxTime) override internal returns (uint256) {
